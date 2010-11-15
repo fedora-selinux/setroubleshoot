961a4e468ec1e65dda174ea3d6c3969e0ebe8641
Dan Walsh <dwalsh@redhat.com>
2010-11-15 14:35:37 -0500

    Add booleans translations

--- plugins/booleans.py
+++ plugins/booleans.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 import seobject
+fd = open('booleans_trans.py', 'w')
 for i in seobject.booleans_dict:
     desc = seobject.booleans_dict[i][2]
-    print "If you want to " + desc[0].lower() + desc[1:]
+    fd.write('_("If you want to ' + desc[0].lower() + desc[1:] + '")\n')
+fd.close()
 #    print "Then you must tell SELinux about this by enabling the %s boolean" % i
 #    print "Do # setsebool -P %s 1 " % i
 #    print "======================================================================"
