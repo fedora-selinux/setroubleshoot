913e252a30040b5750138cf38edebecb38a2f9ea
Dan Walsh <dwalsh@redhat.com>
2012-11-13 07:20:02 -0500

    Fix update translations

--- plugins/booleans.py
+++ plugins/booleans.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python
-import seobject
-fd = open('booleans_trans.py', 'w')
-for i in seobject.booleans_dict:
-    desc = seobject.booleans_dict[i][2]
-    fd.write('_("If you want to ' + desc[0].lower() + desc[1:] + '")\n')
-fd.close()
-#    print "Then you must tell SELinux about this by enabling the %s boolean" % i
-#    print "Do # setsebool -P %s 1 " % i
-#    print "======================================================================"
-#    print desc[0].lower() + desc[1:]
+import sepolicy
+sepolicy.gen_bool_dict()
+for i in sepolicy.booleans_dict:
+    desc = sepolicy.booleans_dict[i][2]
+    print('_("If you want to ' + desc[0].lower() + desc[1:] + '")\n')
 
