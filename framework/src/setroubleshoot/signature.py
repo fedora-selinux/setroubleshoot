30f460c089c5eb659ad2682a96a113b5ad227222
Ondrej Slamecka <oslameck@redhat.com>
2015-07-27 16:05:58 +0200

    Add Python 3 support in signature.py

--- framework/src/setroubleshoot/signature.py
+++ framework/src/setroubleshoot/signature.py
@@ -20,10 +20,13 @@
 #
 import syslog
 from subprocess import *
-import setroubleshoot.default_encoding_utf8
 import gettext
 translation=gettext.translation('setroubleshoot-plugins', fallback=True)
-_=translation.ugettext
+
+try:
+    _ = translation.ugettext # This raises exception in Python3, succ. in Py2
+except AttributeError:
+    _ = translation.gettext # Python3
 
 __all__ = [
            'SignatureMatch',
