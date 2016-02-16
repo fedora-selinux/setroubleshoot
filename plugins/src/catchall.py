24d7dfb86618fe639819ab862ff4d86d8e765a99
Petr Lautrbach <plautrba@redhat.com>
2016-02-16 17:33:11 +0100

    plugins: Suggest to use ausearch instead of grep
    sgrub@redhat.com: Grep has never been a recommended tool to use on the
    audit logs because it will find only one record of an event and not the
    whole event.

--- plugins/src/catchall.py
+++ plugins/src/catchall.py
@@ -54,7 +54,7 @@ class plugin(Plugin):
 
     then_text = _('You should report this as a bug.\nYou can generate a local policy module to allow this access.')
     do_text = _("""Allow this access for now by executing:
-# grep $SOURCE /var/log/audit/audit.log | audit2allow -M mypol
+# ausearch -c $SOURCE --raw | audit2allow -M mypol
 # semodule -i mypol.pp""")
 
     def __init__(self):
