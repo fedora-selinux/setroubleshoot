5bc7625b06ebaddf9091b932a98124ebf8c8ca46
Petr Lautrbach <plautrba@redhat.com>
2016-06-22 13:51:02 +0200

    framework: Catch all subprocess exceptions
    Some exceptions were not correctly handled so that setroubleshootd could
    stop work, e.g., in a case where there's no 'locate' in the system
    
    Fixes regression brought by e69378d7:
    
      File "/usr/lib64/python2.7/subprocess.py", line 568, in check_output
        process = Popen(stdout=PIPE, *popenargs, **kwargs)
      File "/usr/lib64/python2.7/subprocess.py", line 711, in __init__
        errread, errwrite)
      File "/usr/lib64/python2.7/subprocess.py", line 1327, in _execute_child
        raise child_exception
    OSError: [Errno 2] No such file or directory

--- framework/src/setroubleshoot/audit_data.py
+++ framework/src/setroubleshoot/audit_data.py
@@ -876,7 +876,7 @@ class AVC:
                                     break
                             except:
                                 pass
-                    except subprocess.CalledProcessError as e:
+                    except:
                         pass
 
         if path is not None:
