98f053f4c218ac2a956e5ed14afe9e68b513711e
Petr Lautrbach <plautrba@redhat.com>
2017-06-13 14:49:07 +0200

    plugins: Remove unused booleans.py

--- plugins/booleans.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-import sepolicy
-sepolicy.gen_bool_dict()
-for i in sepolicy.booleans_dict:
-    desc = sepolicy.booleans_dict[i][2]
-    print('_("If you want to ' + desc[0].lower() + desc[1:] + '")\n')
-
