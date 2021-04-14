56cf1525b5ebeb3591d4a3ded5299fe82d0f9208
Petr Lautrbach <plautrba@redhat.com>
2021-04-14 17:03:39 +0200

    Stop SetroubleshootFixit after 10 seconds of inactivity

--- framework/src/SetroubleshootFixit.py
+++ framework/src/SetroubleshootFixit.py
@@ -7,6 +7,7 @@ from gi.repository import GLib
 import slip.dbus.service
 from slip.dbus import polkit
 import os
+import signal
 
 
 class RunFix(slip.dbus.service.Object):
@@ -14,12 +15,20 @@ class RunFix(slip.dbus.service.Object):
 
     def __init__(self, *p, **k):
         super(RunFix, self).__init__(*p, **k)
+        self.timeout = 10
+        self.alarm(self.timeout)
+
+    def alarm(self, timeout=10):
+        signal.alarm(timeout)
+
 
     @dbus.service.method("org.fedoraproject.SetroubleshootFixit", in_signature='ss', out_signature='s')
     def run_fix(self, local_id, analysis_id):
         import subprocess
+        self.alarm(0)
         command = ["sealert", "-f", local_id, "-P", analysis_id]
         return subprocess.check_output(command, universal_newlines=True)
+        self.alarm(self.timeout)
 
 if __name__ == "__main__":
     mainloop = GLib.MainLoop()
